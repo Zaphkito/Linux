3fba50b53e930d32d9f9f3f29574b4c647d51429
遠坂 さとう <64299266+Zaphkito@users.noreply.github.com>
2022-02-15 03:46:45 +0900

    Update buildopenwrt.sh

--- Source/OpenWrt/buildopenwrt.sh
+++ Source/OpenWrt/buildopenwrt.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-cd /home/zaphkito/Source/OpenWrt
-git pull
-make clean
+cd /home/zaphkito/Source
+git clone https://github.com/coolsnowwolf/lede OpenWrt
+cd OpenWrt
+echo "src-git openclash https://github.com/vernesong/OpenClash" >> ./feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> ./feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
+wget https://github.com/Zaphkito/Linux/blob/master/Source/OpenWrt/.config
 make defconfig
 make -j8 download V=s
 make -j$(nproc) || make -j1 || make -j1 V=s
