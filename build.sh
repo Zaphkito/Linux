00cec3105a6d06f54eebf7355df0a968929c3fc8
遠坂 さとう <64299266+Zaphkito@users.noreply.github.com>
2022-02-19 23:01:20 +0900

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install lm-sensors vim neofetch npm openssh-server net-tools build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo service cron start
+sudo systemctl enable cron
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+cd /home/zaphkito
+git clone https://github.com/coolsnowwolf/lede Source/OpenWrt
+cd Source
+wget wget https://github.com/Zaphkito/Linux/raw/main/Source/buildopenwrt.sh
+sh buildopenwrt.sh
+crontab -e
