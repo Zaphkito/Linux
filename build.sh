f857e94ce7782d5f9910397e7ac57dc4c9f1afdc
遠坂 さとう <64299266+Zaphkito@users.noreply.github.com>
2022-02-22 05:18:04 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install lm-sensors vim neofetch npm openssh-server net-tools build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo apt-get -y install aria2 qbittorrent lm-sensors vim neofetch npm openssh-server net-tools build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo npm install -g t-get
 sudo service cron start
 sudo systemctl enable cron
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo wget https://issuepcdn.baidupcs.com/issue/netdisk/LinuxGuanjia/4.3.0/baidunetdisk_4.3.0_amd64.deb
+sudo dpkg -i baidunetdisk_4.3.0_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
+rm -rf baidunetdisk_4.3.0_amd64.deb
 cd /home/zaphkito
 git clone https://github.com/coolsnowwolf/lede Source/OpenWrt
 cd Source
