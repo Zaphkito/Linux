4ee9752612de10799c92742c1c094ac7edfee34a
遠坂 さとう <64299266+Zaphkito@users.noreply.github.com>
2022-02-25 04:52:11 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,16 @@ sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo wget https://issuepcdn.baidupcs.com/issue/netdisk/LinuxGuanjia/4.3.0/baidunetdisk_4.3.0_amd64.deb
 sudo dpkg -i baidunetdisk_4.3.0_amd64.deb
+sudo wget https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_1.2.0.132_amd64.deb
+sudo dpkg -i com.alibabainc.dingtalk_1.2.0.132_amd64.deb
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
 rm -rf google-chrome-stable_current_amd64.deb
 rm -rf baidunetdisk_4.3.0_amd64.deb
 rm -rf install.sh
+rm -rf com.alibabainc.dingtalk_1.2.0.132_amd64.deb
 cd /home/zaphkito
 git clone https://github.com/coolsnowwolf/lede Source/OpenWrt
 cd Source
-wget wget https://github.com/Zaphkito/Linux/raw/main/Source/buildopenwrt.sh
+wget https://github.com/Zaphkito/Linux/raw/main/Source/buildopenwrt.sh
 sh buildopenwrt.sh
 crontab -e
